5d9854aa9931f0d63fcd8f32bbd3e72b27af8f05
shafiq005 <126949303+shafiq005@users.noreply.github.com>
2023-03-08 15:26:00 +0530

    Create help1.js

---
+++ src/file/help1.js
@@ -0,0 +1,13 @@
+function Help1(list,k) 
+{
+    for (var i=0 ; i<list.length ; i++)
+    {
+        if(list[i] == k)
+        {
+            return (false)
+        }
+    }
+    return (true)
+}
+
+export default Help1
